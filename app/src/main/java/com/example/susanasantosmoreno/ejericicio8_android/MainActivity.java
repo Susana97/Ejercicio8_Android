548c00e5f2f23de31475cad8aaeb1c613bc74d12
Susana Santos Moreno <susiylola@gmail.com>
2018-11-11 13:52:05 +0100

    Menu-Bar implementado
    Opcion Info sin implementar, tiene que salir un DialogFragment indicando el autor y la fecha de implementación del proyecto.

--- app/src/main/java/com/example/susanasantosmoreno/ejericicio8_android/MainActivity.java
+++ app/src/main/java/com/example/susanasantosmoreno/ejericicio8_android/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.susanasantosmoreno.ejericicio8_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
